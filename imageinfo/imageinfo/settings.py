963968773b06014859836610ae6c223eea8976f8
fahadamin2288 <mrfahad2288@gmail.com>
2025-01-23 14:07:00 +0500

    Fix ALLOWED_HOSTS for Render

--- imageinfo/imageinfo/settings.py
+++ imageinfo/imageinfo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['image-uploader.onrender.com']
+
 
 
 # Application definition
