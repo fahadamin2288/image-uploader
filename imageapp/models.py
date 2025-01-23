0f81077481ef294a1f6f463901de575ed4f6c310
Fahad Amin <mrfahad2288@gmail.com>
2025-01-23 07:48:13 +0500

    Delete imageapp directory

--- imageapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Image(models.Model):
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images')
-    date = models.DateTimeField(auto_now_add=True)
