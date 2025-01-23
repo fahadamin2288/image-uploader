c4268d352ad2046dcb52bffe54371255e9d2b3a2
fahadamin2288 <mrfahad2288@gmail.com>
2025-01-23 07:37:29 +0500

    Update Django settings

--- imageinfo/imageinfo/settings.py
+++ imageinfo/imageinfo/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'imageapp'
+    'imageapp',
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
